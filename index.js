8fdf14a865ced80cc7509690ca0f7893966056e0
John Karrell Speck <speckworks@gmail.com>
2019-07-24 22:33:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return
+  string.toUpperCase()
+  
+}
+
+function whisper(string){ return string.toLowerCase()}
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
